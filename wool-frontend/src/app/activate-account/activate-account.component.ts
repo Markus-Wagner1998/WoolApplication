608a788982c6fa146092f9962e04595be0722a58
Markus Wagner <markus.wagner1998@gmail.com>
2023-07-23 12:28:43 +0200

    Account Activation Frontend Component

---
+++ wool-frontend/src/app/activate-account/activate-account.component.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-activate-account',
+  templateUrl: './activate-account.component.html',
+  styleUrls: ['./activate-account.component.css']
+})
+export class ActivateAccountComponent implements OnInit {
+  message: string = 'Account wird aktiviert;'
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((queryParams) => {
+      if (queryParams['hash']) {
+        const currentUserhash: string = queryParams['hash'];
+        this.http.get('/api/auth/activate/' + currentUserhash).subscribe((data) => {
+          this.router.navigate(['/login'], {
+            queryParams: {
+              active: true,
+            },
+          });
+        })
+      } else {
+        this.router.navigate(['/login']);
+      } 
+    })
+  }
+}
